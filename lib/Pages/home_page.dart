31d5e5a918e581072a65a70b6b78cc2ef1b9e304
Satyam Gupta <30satyamgupta@gmail.com.com>
2022-10-01 18:56:10 +0530

    Day 10 completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -8,7 +8,7 @@ import '../widgets/Item_widgets.dart';
 import '../widgets/drawer.dart';
 
 class HomePage extends StatefulWidget {
-  // const HomePage({ Key key }) : super(key: key);
+  const HomePage({ Key key }) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -49,12 +49,46 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)? ListView.builder(
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)     
+        ? GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+            ),
+          itemBuilder: (context,index) {
+            final item = CatalogModel.items[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)),       
+              child: GridTile(
+                header: Container(
+                  child: Text(item.name , style: TextStyle(color: Colors.white),),
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                  ),
+                  ),
+                child: Image.network(item.image),
+                footer: Container(
+                  child: Text(item.price.toString() , style: TextStyle(color: Colors.white),),
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                  ),
+                  ),
+              ),
+            );
+          },
           itemCount: CatalogModel.items.length,
-          itemBuilder:(context, index) => ItemWidget(
-              item: CatalogModel.items[index],
-              ),   
-        )
+          )
+        //   ListView.builder(
+        //   itemCount: CatalogModel.items.length,
+        //   itemBuilder:(context, index) => ItemWidget(
+        //       item: CatalogModel.items[index],
+        //       ),   
+        // )
         :Center(
           child:  CircularProgressIndicator(),
         ),
